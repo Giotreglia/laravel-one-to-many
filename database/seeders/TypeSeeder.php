7a30eb25644eadf29ac4c469fa0a62c5fe6d939d
Giotreglia <giovanni.treglia@gmail.com>
2023-05-24 15:57:26 +0200

    Nuova entità Type creata

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ['WebApp', 'Game', 'Website', 'MobileApp'];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($type, '-');
+            $newType->save();
+        }
     }
 }
